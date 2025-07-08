24c4cfe8c697967ee39a94890b1c33bf71cf5e5f
artha <arthaparulian31@gmail.com>
2025-07-08 16:35:03 +0700

    test

---
+++ js/maps.js
@@ -0,0 +1,61 @@
+// The Map Object
+// A Map object holds key-value pairs where the keys can be any datatype.
+
+// A Map also remembers the original insertion order of the keys.
+
+// How to Create a Map
+// You can create a JavaScript Map by:
+
+// Passing an Array to new Map()
+// Create a Map and use Map.set()
+// The new Map() Method
+// You can create a Map by passing an Array to the new Map() constructor:
+
+// Example
+// // Create a Map
+// const fruits = new Map([
+//   ["apples", 500],
+//   ["bananas", 300],
+//   ["oranges", 200]
+// ]);
+// The set() Method
+// You can add elements to a Map with the set() method:
+
+// Example
+// // Create a Map
+// const fruits = new Map();
+
+// // Set Map Values
+// fruits.set("apples", 500);
+// fruits.set("bananas", 300);
+// fruits.set("oranges", 200);
+// The set() method can also be used to change existing Map values:
+
+// Example
+// fruits.set("apples", 200);
+// The get() Method
+// The get() method gets the value of a key in a Map:
+
+// Example
+// fruits.get("apples");    // Returns 500
+// ADVERTISEMENT
+
+// Maps are Objects
+// typeof returns object:
+
+// Example
+// // Returns object:
+// typeof fruits;
+// instanceof Map returns true:
+
+// Example
+// // Returns true:
+// fruits instanceof Map;
+// JavaScript Objects vs Maps
+// Differences between JavaScript Objects and Maps:
+// Object	Map
+// Not directly iterable	Directly iterable
+// Do not have a size property	Have a size property
+// Keys must be Strings (or Symbols)	Keys can be any datatype
+// Keys are not well ordered	Keys are ordered by insertion
+// Have default keys	Do not have default keys
---
+++ js/object-method.js
@@ -0,0 +1,53 @@
+// Object methods are actions that can be performed on objects.
+
+// A method is a function definition stored as a property value.
+
+// Property	Value
+// firstName	John
+// lastName	Doe
+// age	50
+// eyeColor	blue
+// fullName	function() {return this.firstName + " " + this.lastName;}
+// Example
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+// In the example above, this refers to the person object:
+
+// this.firstName means the firstName property of person.
+
+// this.lastName means the lastName property of person.
+
+// Accessing Object Methods
+// You access an object method with the following syntax:
+
+// objectName.methodName()
+// If you invoke the fullName property with (), it will execute as a function:
+
+// Example
+// name = person.fullName();
+// If you access the fullName property without (), it will return the function definition:
+
+// Example
+// name = person.fullName;
+// ADVERTISEMENT
+
+// Adding a Method to an Object
+// Adding a new method to an object is easy:
+
+// Example
+// person.name = function () {
+//   return this.firstName + " " + this.lastName;
+// };
+// Using JavaScript Methods
+// This example uses the JavaScript toUpperCase() method to convert a text to uppercase:
+
+// Example
+// person.name = function () {
+//   return (this.firstName + " " + this.lastName).toUpperCase();
+// };
